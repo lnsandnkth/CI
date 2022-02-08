e58c40459c9ec7572196e8caeb54973c0757c659
xinmiao <>
2022-02-08 23:58:52 +0100

    feat - add db table create (#9)

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,7 +1,13 @@
 import com.example.database.Database;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 
 public class DatabaseTest {
+    @BeforeAll
     static void setup() {
         Database.connect("test.db");
     }
+
 }
