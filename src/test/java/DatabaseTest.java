daec3949f28c7166a2f81b9cba1a69587526ec13
xinmiao <>
2022-02-08 21:13:09 +0100

    feat - add sqlite-jdbc (#7)

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -2,7 +2,6 @@ import com.example.database.Database;
 
 public class DatabaseTest {
     static void setup() {
-        Database db = new Database();
-        Database.connect();
+        Database.connect("test.db");
     }
 }
