353cbb455d3b114c908bca4c9f9e46167a7b6dc9
xinmiao <unknown>
2022-02-08 23:58:52 +0100

    feat - add db table create (#9)

--- src/main/java/com/example/database/Database.java
+++ src/main/java/com/example/database/Database.java
@@ -5,10 +5,15 @@ import java.sql.*;
 public class Database {
     static Connection c = null;
 
+    /**
+     * connect to the database
+     * @param path specify the path of db
+     */
     public static void connect(String path) {
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:" + path);
+            createTable();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
@@ -16,6 +21,9 @@ public class Database {
         System.out.println("Opened database successfully");
     }
 
+    /**
+     * disconnect to database
+     */
     public static void disconnect() {
         try{
             if (c != null) {
@@ -24,6 +32,43 @@ public class Database {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+
+    /**
+     * create a new table if not exist
+     */
+    public static void createTable() {
+        String sql = """
+                CREATE TABLE builds (
+                        uid INTEGER PRIMARY KEY,
+                        commit_hash TEXT,
+                        content TEXT,
+                        timestamp TIMESTAMP
+                );
+                """;
+        try {
+            Statement createstat = c.createStatement();
+            String checkTable = "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='builds'";
+            ResultSet rs = createstat.executeQuery(checkTable);
+            if (rs.next())
+            {
+                int exists = rs.getInt(1);
+                if (exists == 0)
+                {
+                    System.err.println("Added builds table as it did not exist");
+                    Statement exestat = c.createStatement();
+                    exestat.execute(sql);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    //TODO the input data type should be specified as issue#20 / 22 ?
+    public void addInfo() {
+
     }
 
 
