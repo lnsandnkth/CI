0d9d643453b7ec49ce24f57bb0fa4dec6866c2d5
Maxou <5208681+mxyns@users.noreply.github.com>
2022-02-09 02:32:48 +0100

    test - added tests for GradleProject (#23)

---
+++ src/test/resources/CI_test_pass/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook See the Jetty documentation for API documentation of
+ * those classes.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Passing World!");
+    }
+}
