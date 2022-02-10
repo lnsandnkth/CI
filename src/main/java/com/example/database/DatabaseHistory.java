3e7812ff520c2062ebf22d86c9d54e849daf5307
Maxou <5208681+mxyns@users.noreply.github.com>
2022-02-10 17:48:28 +0100

    feat - added link to commit details page on full history page (#42)

--- src/main/java/com/example/database/DatabaseHistory.java
+++ src/main/java/com/example/database/DatabaseHistory.java
@@ -14,29 +14,28 @@ public class DatabaseHistory{
                 <!DOCTYPE html>
                 <html>
                 <body>
-                <div style=\"max-width: 1000px; margin-left: auto; margin-right: auto; padding-left: 10px; padding-right: 10px;\">
-                  <h2 style=\"font-size: 26px; margin: 20px 0; text-align: center; small { font-size: 0.5em; }\">Build Info</h2>
+                <div style="max-width: 1000px; margin-left: auto; margin-right: auto; padding-left: 10px; padding-right: 10px;">
+                  <h2 style="font-size: 26px; margin: 20px 0; text-align: center; small { font-size: 0.5em; }">Build Info</h2>
                   <ul>
-                     <li style=\"border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #95A5A6; font-size: 14px; text-transform: uppercase; letter-spacing: 0.03em;\">
-                       <div style=\"flex-basis: 20%;margin:20px;\">commit identifier</div>
-                       <div style=\"flex-basis: 10%;margin:20px;\">build date</div>
-                       <div style=\"flex-basis: 70%;margin:20px;\">build logs</div>
+                     <li style="border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #95A5A6; font-size: 14px; text-transform: uppercase; letter-spacing: 0.03em;">
+                       <div style="flex-basis: 20%;margin:20px;">commit identifier</div>
+                       <div style="flex-basis: 10%;margin:20px;">build date</div>
+                       <div style="flex-basis: 70%;margin:20px;">build logs</div>
                      </li>
                      """;
                      for(BuildInfo bi : bis){
                         out += """
-                                <li style=\"border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #ffffff;box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);\">
-                                  <div style=\"flex-basis: 20%;margin:20px;\">
-                                      """;
-                        out += bi.getCommit_id();
+                                <li style="border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #ffffff;box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);">
+                                  <a href="/buildinfo?commit_id=""" + bi.getCommit_id() + "\">";
+                        out += bi.getCommit_id().subSequence(0, Math.min(bi.getCommit_id().length(), 8));
                         out += """
-                                </div>
-                                <div style=\"flex-basis: 10%;margin:20px;\">
+                                </a>
+                                <div style="flex-basis: 10%;margin:20px;">
                                     """;
                         out += bi.getBuild_date();
                         out += """
                                 </div>
-                                <div style=\"flex-basis: 70%;margin:20px;\">
+                                <div style="flex-basis: 70%;margin:20px;">
                                     """;
                         out += bi.getLogs();
                         out += """
@@ -65,26 +64,26 @@ public class DatabaseHistory{
             <!DOCTYPE html>
            <html>
            <body>
-           <div style=\"max-width: 1000px; margin-left: auto; margin-right: auto; padding-left: 10px; padding-right: 10px;\">
-                 <h2 style=\"font-size: 26px; margin: 20px 0; text-align: center; small { font-size: 0.5em; }\">Build Info</h2>
+           <div style="max-width: 1000px; margin-left: auto; margin-right: auto; padding-left: 10px; padding-right: 10px;">
+                 <h2 style="font-size: 26px; margin: 20px 0; text-align: center; small { font-size: 0.5em; }">Build Info</h2>
                  <ul>
-                   <li style=\"border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #95A5A6; font-size: 14px; text-transform: uppercase; letter-spacing: 0.03em;\">
-                     <div style=\"flex-basis: 20%;margin:20px;\">commit identifier</div>
-                     <div style=\"flex-basis: 10%;margin:20px;\">build date</div>
-                     <div style=\"flex-basis: 70%;margin:20px;\">build logs</div>
+                   <li style="border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #95A5A6; font-size: 14px; text-transform: uppercase; letter-spacing: 0.03em;">
+                     <div style="flex-basis: 20%;margin:20px;">commit identifier</div>
+                     <div style="flex-basis: 10%;margin:20px;">build date</div>
+                     <div style="flex-basis: 70%;margin:20px;">build logs</div>
                    </li>
-                   <li style=\"border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #ffffff;box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);\">
-                     <div style=\"flex-basis: 20%;margin:20px;\">
+                   <li style="border-radius: 3px; padding: 25px 30px; display: flex; justify-content: space-between; margin-bottom: 25px;background-color: #ffffff;box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);">
+                     <div style="flex-basis: 20%;margin:20px;">
                          """;
                          out+=bi.getCommit_id();
                          out+="""
                      </div>
-                     <div style=\"flex-basis: 10%;margin:20px;\">
+                     <div style="flex-basis: 10%;margin:20px;">
                          """;
                          out+=bi.getBuild_date();
                          out+= """
                      </div>
-                     <div style=\"flex-basis: 70%;margin:20px;\">
+                     <div style="flex-basis: 70%;margin:20px;">
                          """;
                          out+=bi.getLogs();
                          out+="""
