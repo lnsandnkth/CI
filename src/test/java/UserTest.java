b3fddf710127b159075437add38998f155f2d493
Maxou <5208681+mxyns@users.noreply.github.com>
2022-02-15 16:54:26 +0100

    fixed purposely failing test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -275,7 +275,7 @@ public class UserTest {
                 Assertions.assertEquals(committer.name, "web-flow");
                 Assertions.assertEquals(committer.email, "noreply@github.com");
                 Assertions.assertEquals(committer.avatarUrl, "");
-                Assertions.assertEquals(committer.htmlUrl, "sdf");
+                Assertions.assertEquals(committer.htmlUrl, "");
                 System.out.println(committer);
             }
                                      );
